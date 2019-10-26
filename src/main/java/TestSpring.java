06fda99fcc0de756f34bade3328f0c6f83d448e7
nick <gnn@mm.st>
2019-10-26 17:21:06 +0300

    add DI

--- src/main/java/TestSpring.java
+++ src/main/java/TestSpring.java
@@ -3,10 +3,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("appContext.xml");
-        Music music = context.getBean("musicBean", Music.class);
 
-        MusicPlayer player = new MusicPlayer(music);
+//        Music music = context.getBean("musicBean", Music.class);
 
+        MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class);
         player.playMusic();
 
         context.close();
