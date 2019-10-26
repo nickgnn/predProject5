468de69090cf71bd25744b90cc2fd9e8ac4bae2c
nick <gnn@mm.st>
2019-10-26 17:18:16 +0300

    add IoC

---
+++ src/main/java/RockMusic.java
@@ -0,0 +1,6 @@
+public class RockMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Bohemian Rhapsody";
+    }
+}
--- src/main/java/TestSpring.java
+++ src/main/java/TestSpring.java
@@ -3,9 +3,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("appContext.xml");
-        TestBean testBean = context.getBean("testBean", TestBean.class);
+        Music music = context.getBean("musicBean", Music.class);
 
-        System.out.println(testBean.getName());
+        MusicPlayer player = new MusicPlayer(music);
+
+        player.playMusic();
 
         context.close();
     }
