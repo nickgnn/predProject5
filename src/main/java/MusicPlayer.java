6e6165e8f7db096b87a928e7f7a49d84b0c11e9e
nick <gnn@mm.st>
2019-10-27 13:59:59 +0300

    add DI part2

--- src/main/java/MusicPlayer.java
+++ src/main/java/MusicPlayer.java
@@ -1,8 +1,38 @@
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+    private int volume;
+
+    public MusicPlayer() {
+    }
+
     public MusicPlayer(Music music) {
         this.music = music;
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 
     public void playMusic() {
--- src/main/java/TestSpring.java
+++ src/main/java/TestSpring.java
@@ -9,6 +9,9 @@ public class TestSpring {
         MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class);
         player.playMusic();
 
+        System.out.println(player.getName());
+        System.out.println(player.getVolume());
+
         context.close();
     }
 }
