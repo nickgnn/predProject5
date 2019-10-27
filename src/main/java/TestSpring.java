1de381b48c3c1379b11b3f7a2d4759edb11aad2e
nick <gnn@mm.st>
2019-10-27 22:56:15 +0300

    check scope(singleton or prototype)

--- src/main/java/TestSpring.java
+++ src/main/java/TestSpring.java
@@ -4,13 +4,20 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("appContext.xml");
 
-//        Music music = context.getBean("musicBean", Music.class);
+        MusicPlayer player1 = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer player2 = context.getBean("musicPlayer", MusicPlayer.class);
 
-        MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class);
-        player.playMusic();
+        boolean comparison = player1 == player2;
 
-        System.out.println(player.getName());
-        System.out.println(player.getVolume());
+        System.out.println(comparison);
+
+        System.out.println(player1);
+        System.out.println(player2);
+
+        player1.setVolume(10);
+
+        System.out.println(player1.getVolume());
+        System.out.println(player2.getVolume());
 
         context.close();
     }
