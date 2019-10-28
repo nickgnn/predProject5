6d8ea5b48a4e529fa6d762fef89e70c62d872f45
nick <gnn@mm.st>
2019-10-28 19:16:17 +0300

    add do-init do-destroy

--- src/main/java/ClassicMusic.java
+++ src/main/java/ClassicMusic.java
@@ -1,4 +1,12 @@
 public class ClassicMusic implements Music {
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
--- src/main/java/TestSpring.java
+++ src/main/java/TestSpring.java
@@ -4,20 +4,24 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("appContext.xml");
 
-        MusicPlayer player1 = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer player2 = context.getBean("musicPlayer", MusicPlayer.class);
+        ClassicMusic classicMusic = context.getBean("musicBeanClassic", ClassicMusic.class);
 
-        boolean comparison = player1 == player2;
+        System.out.println(classicMusic.getSong());
 
-        System.out.println(comparison);
-
-        System.out.println(player1);
-        System.out.println(player2);
-
-        player1.setVolume(10);
-
-        System.out.println(player1.getVolume());
-        System.out.println(player2.getVolume());
+//        MusicPlayer player1 = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer player2 = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean comparison = player1 == player2;
+//
+//        System.out.println(comparison);
+//
+//        System.out.println(player1);
+//        System.out.println(player2);
+//
+//        player1.setVolume(10);
+//
+//        System.out.println(player1.getVolume());
+//        System.out.println(player2.getVolume());
 
         context.close();
     }
